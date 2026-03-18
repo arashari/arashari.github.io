245ce0a55dae3887e9a4d2c6c35ad43ba208340f
Ahmad Rifai Ashari <email.arashari@gmail.com>
2026-03-18 13:57:03 +0700

    feat: add service worker for offline caching

---
+++ mathcat-helper/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'mathcat-v1';
+const PRECACHE_URLS = ['./', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE_URLS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
